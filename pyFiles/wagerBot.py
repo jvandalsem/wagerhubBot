825784d424e8b47d8a193182ab7cfac3e0d10b06
jvandalsem <jjvandal@umich.edu>
2021-05-04 20:37:12 -0700

    Fixed clicking radio fields for all sports

--- pyFiles/wagerBot.py
+++ pyFiles/wagerBot.py
@@ -6,6 +6,7 @@ from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import os
+import time
 # os.environ['MOZ_HEADLESS'] = '1'
 class formDriver:
     def __init__(self):
@@ -46,25 +47,31 @@ class formDriver:
     def getSports(self):
         print('You can bet on the NBA, MLB, NHL, and EPL')
 
-    def selectSport(self,sport='NHL'):
-        if sport.lower() == 'nba':
-            self.driver.find_element_by_id('lg_3').click()
-            self.driver.find_element(By.TAG_NAME, 'body').send_keys(Keys.RETURN)
-        elif sport.lower() == 'mlb':
-            WebDriverWait(self.driver,10).until(EC.invisibility_of_element_located((By.XPATH,"//div[@class='col-12 text-center']")))
-            el_xp('//*[@id="lg_1166"]').click()
-            # element = self.driver.find_element_by_xpath("//div[@class='col-12 text-center']")
-            # self.driver.execute_script("arguments[0].style.visibility='hidden'", element)
-            # self.driver.find_element_by_xpath('//*[@id="lg_1166"]').click()
-            self.driver.find_element(By.TAG_NAME, 'body').send_keys(Keys.RETURN)
-        elif sport.lower() == 'nhl':
-            self.driver.find_element_by_xpath("//label[@for='lg_1166']").click()
-            self.driver.find_element(By.TAG_NAME, 'body').send_keys(Keys.RETURN)
-        elif sport.lower() == 'epl':
-            self.driver.find_element_by_id('lg_1699').click()
-            self.driver.find_element(By.TAG_NAME, 'body').send_keys(Keys.RETURN)
-        else:
-            print("I'm sorry, we don't offer the functionality to place wagers on that sport yet")
+    def selectSport(self,sport='nba'):
+        try:
+            if sport.lower() == 'nba':
+                id = 'lg_3'
+            elif sport.lower() == 'mlb':
+                id = 'lg_5'
+            elif sport.lower() == 'nhl':
+                id = 'lg_1166'
+            elif sport.lower() == 'epl':
+                id = 'lg_1699'
+            else:
+                print("I'm sorry, we don't offer the functionality to place wagers on that sport yet")
+
+            radioElem = self.driver.find_element_by_xpath("//*[@id='%s']"%id)
+            self.driver.execute_script("arguments[0].click()",radioElem)
+            contElem = self.driver.find_element_by_xpath("//input[@value='Continue']")
+            self.driver.execute_script("arguments[0].click()",contElem)
+
+        except:
+            print('Looks like there are no games available for that sport right now')
     def test1(self):
         element = self.driver.find_element_by_xpath("//div[@class='col-12 text-center']")
         print(element)
+#
+# WebDriverWait(self.driver,10).until(EC.invisibility_of_element_located((By.XPATH,"//div[@class='col-12 text-center']")))
+# el_xp('//*[@id="lg_1166"]').click()
+# element = self.driver.find_element_by_xpath("//div[@class='col-12 text-center']")
+# self.driver.execute_script("arguments[0].style.visibility='hidden'", element)
