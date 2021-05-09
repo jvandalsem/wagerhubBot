af558d88f9114f772f39571216abe9a4f73e3d82
jvandalsem <jjvandal@umich.edu>
2021-05-08 23:57:43 -0700

    changed `games` command to lines

--- pyFiles/betslave.py
+++ pyFiles/betslave.py
@@ -1,9 +1,12 @@
 import os
 import discord
-from dotenv import load_dotenv
 from discord.ext import commands
+from dotenv import load_dotenv
 from wagerBot import formDriver
-from helpers import formatMD_GS
+from helpers import *
+from liveScores import scrapetheScore
+import teams
+import pandas
 os.environ['MOZ_HEADLESS'] = '1'
 
 wager = formDriver()
@@ -16,23 +19,40 @@ help_command = commands.DefaultHelpCommand(no_category = 'Commands')
 bot = commands.Bot(command_prefix=commands.when_mentioned_or('$'),
                    help_command=help_command)
 
-@bot.command(name='OpenBets',brief='-> Your open bets')
+@bot.command(name='open',brief='-> Your open bets')
 async def bot_openBets(ctx):
     await ctx.send(wager.getOpenBets())
 
-@bot.command(name='Sports',brief='-> Sports you can bet on.')
+@bot.command(name='sports',brief='-> Sports you can bet on.')
 async def bot_getSports(ctx):
     await ctx.send(wager.getSports())
 
-@bot.command(name='Balance',brief='-> Your current book balance')
+@bot.command(name='balance',brief='-> Your current book balance')
 async def bot_getSports(ctx):
     await ctx.send(wager.getWinnings())
 
-@bot.command(name='Which',brief='-> Which <sport> games you can bet on')
+@bot.command(name='lines',brief='-> Which <sport> games you can bet on')
 async def bot_getSports(ctx,*args):
     response = formatMD_GS(wager.selectBetType('single').listGames(args[0]))
+    await ctx.send('Master, here are all the {} games you can bet on'.format(args[0].upper()))
     for a in range(len(response)):
         await ctx.send(response[a])
-        print(a)
+
+@bot.command(name='score',brief='-> score <league> <team> vs. <team>')
+async def bot_getSports(ctx,*args):
+    game = [a for a in formatLS(scrapetheScore(args[0].lower())) if args[1] in a or args[1] in a.lower()]
+    print(game)
+    check1 = checkTeams(args[0])
+    if args[1].lower() not in check1:
+        await ctx.send("Master, I don't believe the {} are an {} team.".format(args[1],args[0].upper()))
+    elif len(game)==0:
+        await ctx.send("Master, I don't believe the {} are playing today".format(args[1]))
+    else:
+        await ctx.send(game[0])
+
+@bot.command(name='scores',brief='-> score <league>')
+async def bot_getSports(ctx,*args):
+    for a in formatLS(scrapetheScore(args[0].lower())):
+        await ctx.send(a)
 
 bot.run(TOKEN)
